cc7ea1dd17a24d602a0a6dc8050cc6e652d752a9
Sneha Silwal <ssilwal@users.noreply.github.com>
2016-08-07 05:37:05 -0400

    Getting started with kalman filtering

--- shakefl/shakefl.py
+++ shakefl/shakefl.py
@@ -1,7 +1,9 @@
 import os
+import numpy as np
 
 from flask import Flask, request, g, redirect,render_template, flash, jsonify
 from flask_pymongo import PyMongo
+from pykalman import KalmanFilter
 
 app = Flask("shakefl")
 app.config.from_object(__name__)
@@ -10,6 +12,7 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def show_all():
+    pkm()
     online_all = mongo.db.myColl.find({'username':"tswift"})
     return render_template('index.html',
             online_all=online_all)
@@ -21,3 +24,20 @@ def add_data():
     return jsonify({
         "authenticated":True
     })
+
+def pkm():
+    #https://pykalman.github.io
+    #READ Basic Usage
+    print("Beginning Kalman Filtering....")
+    kf = KalmanFilter(initial_state_mean=0, n_dim_obs=9)
+
+    #measurements - array of 9x1 vals
+    #each a_x, a_y, a_z, g_x, g_y, g_z, m_x, m_y, m_z
+    measurements = getMeasurementsFromDB()
+    smooth_inputs = [[2,0], [2,1], [2,2]]
+    #traditional assumed params are known, but we can get from EM on the measurements
+    #we get better predictions of hidden states (true position) with smooth function
+    kf.em(measurements).smooth(smooth_inputs)
+
+    print(measurements)
+    print(kf.em(measurements, n_iter=5))
